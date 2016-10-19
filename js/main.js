b0eb98f2354cfac35e20698a6b73239d1c462efa
Tom Anderson <portlester.tom@gmail.com>
2016-10-19 21:38:06 +1100

    Added comments

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
+
+    // Sending Dweets
     $("#dweet").click(function() {
+        // Value of input
         var content = $("#message").val();
+        //                  thing                                   content             callback
         dweetio.dweet_for("af62bdb5-92d3-4887-b0f2-d2266c7244e6", {hello: content}, function(err, dweet) {
             console.log(dweet.thing);
             console.log(dweet.content);
