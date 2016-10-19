5586c8e6ff527149877efefba54915eaed240a7b
Tom Anderson <portlester.tom@gmail.com>
2016-10-19 22:17:15 +1100

    Cleaned up JS code

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
-$(document).ready(function() {
-
-    // Sending Dweets
-    $("#dweet").click(function() {
-        // Value of input
-        var content = $("#newMessage").val();
-        //                  thing                                   content             callback
-        dweetio.dweet_for("af62bdb5-92d3-4887-b0f2-d2266c7244e6", {hello: content}, function(err, dweet) {
-            if(err) {
-                console.log(err);
-            }
-        });
+function dweetMessage(content) {
+    //                  thing                                   content             callback
+    dweetio.dweet_for("af62bdb5-92d3-4887-b0f2-d2266c7244e6", {hello: content}, function(err, dweet) {
+        if(err) {
+            console.log(err);
+        }
     });
+}
+function appendMessage(message) {
+    $("#messages").append("<p>" + message + "</p>");
+}
 
+$(document).ready(function() {
     // Listening to Dweets
     dweetio.listen_for("af62bdb5-92d3-4887-b0f2-d2266c7244e6", function(dweet) {
         // Append dweet to messages div
-        $("#messages").append("<p>" + dweet.content["hello"] + "</p>");
+        appendMessage(dweet.content["hello"]);
+    });
+
+    // Sending Dweets
+    $("#dweet").click(function() {
+        // Dweets with value of input
+        dweetMessage($("#newMessage").val());
     });
 });
