be2a480756257401484924ef361eb38122a3f1ae
Tom Anderson <portlester.tom@gmail.com>
2016-10-20 06:29:08 +1100

    JS functionality Dweets send on enter, checks to make sure that the input isn't empty and clears the input after send

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 // Dweets to message1 dweet with specified content
 function dweetMessage(content) {
-    //                  thing                                   content             callback
-    dweetio.dweet_for("af62bdb5-92d3-4887-b0f2-d2266c7244e6", {hello: content}, function(err, dweet) {
-        if(err) {
-            console.log(err);
-        }
-    });
+    // Check to make sure input isn't empty'
+    if(content != "") {
+        //                  thing                                   content             callback
+        dweetio.dweet_for("af62bdb5-92d3-4887-b0f2-d2266c7244e6", {hello: content}, function(err, dweet) {
+            if(err) {
+                console.log(err);
+            }
+            // Emptys input
+            $("#newMessage").val("");
+        });
+    }
 }
 // Appends specified message to messages div
 function appendMessage(message) {
@@ -20,8 +25,15 @@ $(document).ready(function() {
     });
 
     // Sending Dweets
+    // Send Dweet on click of button
     $("#dweet").click(function() {
         // Dweets with value of input
         dweetMessage($("#newMessage").val());
+    });
+    // Send Dweet on enter press
+    $("#newMessage").keydown(function(e) {
+        if(e.which == 13) {
+            dweetMessage($("#newMessage").val());
+        }
     });
 });
