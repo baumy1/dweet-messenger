d63be8177804c05563347af6c87b7e204854df3d
Tom Anderson <portlester.tom@gmail.com>
2016-10-19 21:45:59 +1100

    Listen for dweets

--- js/main.js
+++ js/main.js
@@ -6,10 +6,14 @@ $(document).ready(function() {
         var content = $("#message").val();
         //                  thing                                   content             callback
         dweetio.dweet_for("af62bdb5-92d3-4887-b0f2-d2266c7244e6", {hello: content}, function(err, dweet) {
-            console.log(dweet.thing);
-            console.log(dweet.content);
-            console.log(dweet.created);
-            console.log(err);
+            if(err) {
+                console.log(err);
+            }
         });
+    });
+
+    // Listening to Dweets
+    dweetio.listen_for("af62bdb5-92d3-4887-b0f2-d2266c7244e6", function(dweet) {
+        console.log(dweet.content);
     });
 });
